f6fac92b00f9adc2292e6beb01f108064a26b9b7
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-04-06 10:08:28 -0300

    Código

--- src/Threads.java
+++ src/Threads.java
@@ -1,51 +1,69 @@
 import java.util.concurrent.Semaphore;
 
-public class Threads extends Thread{
-	
+public class Threads extends Thread {
+
 	private int ID;
 	private Semaphore semaforo;
-	
+
 	public Threads(int ID, Semaphore semaforo) {
 		this.ID = ID;
 		this.semaforo = semaforo;
 	}
-	
+
 	public void run() {
 		int valor = ID % 2;
 		switch (valor) {
-			case 0 :
+		case 0:
+			try {
 				sopa();
-				break;
-			case 1:
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			break;
+		case 1:
+			try {
 				lasanha();
-				break;
-			default:
-				System.out.println("Erro!");
-				break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			System.out.println("Erro!");
+			break;
 		}
 	}
-	
-	public void sopa() {
+
+	public void sopa() throws InterruptedException {
 		int tempo = (int) (Math.random() * 301) + 500;
-		for (int i = 1; i <= 6; i++) {
-			double percentual = tempo / 0.1;
-			System.out.println(ID + " - " + percentual + "%");
+		int tempoTotal = tempo / 10;
+		int percentual = 0;
+		for (int i = 0; i <= tempoTotal; i++) {
+			if (i % 10 == 0) {
+				percentual = 10 + percentual;
+				System.out.println("Cozimento do prato #" + ID + ": " + percentual + "%");
+			}
+			Thread.sleep(100);
 		}
 		System.out.println("Prato # " + ID + " pronto! (Sopa de cebola)");
 		entrega();
 	}
-	
-	public void lasanha() {
+
+	public void lasanha() throws InterruptedException {
 		int tempo = (int) (Math.random() * 601) + 600;
-		for (int i = 1; i <= 6; i++) {
-			double percentual = tempo / 0.1;
-			System.out.println(ID + " - " + percentual + "%");
+		int tempoTotal = tempo / 10;
+		int percentual = 0;
+		for (int i = 0; i <= tempoTotal; i++) {
+			if (i % 10 == 0) {
+				percentual = 10 + percentual;
+				System.out.println("Cozimento do prato #" + ID + ": " + percentual + "%");
+			}
+			Thread.sleep(100);
 		}
 		System.out.println("Prato # " + ID + " pronto! (Lasanha a Bolonhesa)");
 		entrega();
-		
+
 	}
-	
+
 	public void entrega() {
 		int tempo = 500;
 		try {
@@ -57,6 +75,6 @@ public class Threads extends Thread{
 		} finally {
 			semaforo.release();
 		}
-		
+
 	}
 }
